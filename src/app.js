d5ed6c10359f526cfaf00d5af9718eb0e40d0ca2
Daniela Maica <danielamaica43@gmail.com>
2025-07-22 19:53:53 +0000

    Proyecto Generador de Excusas en /src

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+// Arrays con partes de la excusa
+let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi papá'];
+let action = ['se comió', 'orinó en', 'rompió', 'aplastó'];
+let what = ['mi tarea', 'mi celular', 'el coche', 'la computadora'];
+let when = ['antes de clase', 'mientras dormía', 'durante el almuerzo', 'cuando estaba rezando'];
 
+// Función para generar excusa
+function generarExcusa() {
+  let parte1 = who[Math.floor(Math.random() * who.length)];
+  let parte2 = action[Math.floor(Math.random() * action.length)];
+  let parte3 = what[Math.floor(Math.random() * what.length)];
+  let parte4 = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${parte1} ${parte2} ${parte3} ${parte4}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Al cargar la página, genera y muestra una excusa
+window.onload = function () {
+  const excusaElemento = document.getElementById('excuse');
+  excusaElemento.innerHTML = generarExcusa();
 };
